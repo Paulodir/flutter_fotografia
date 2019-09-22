c6f5a574f9c7eeb3e4ab42b13292c830b5ea6d08
Paulodir <paulodir.oliver@gmail.com>
2019-09-21 22:28:30 -0300

    Commit Inicial

---
+++ android/app/src/main/kotlin/paulodir/com/br/flutter_fotografia/MainActivity.kt
@@ -0,0 +1,13 @@
+package paulodir.com.br.flutter_fotografia
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
